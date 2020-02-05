72bd0bdfdb3d4292af6486e56d556d729b5e69e7
Rúben Silva <rubensilva945@gmail.com>
2020-02-05 21:02:29 +0000

    feat(info): removed info

--- src/model/api_info.js
+++ src/model/api_info.js
@@ -1,7 +1,7 @@
 export default {
     baseApiUrl: 'https://api.themoviedb.org/3/',
     imagesPathApi: 'http://image.tmdb.org/t/p/w500',
-    api_key: '2c61870adb015c155fc0eb2f648585a5',
+    api_key: '',
     discoverMovieEndpoint: function () {
         return `${this.baseApiUrl}discover/movie?primary_release_year=2010&sort_by=vote_average.desc&sort_by=popularity.desc&api_key=${this.api_key}`;
     },
